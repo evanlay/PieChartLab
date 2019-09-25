2a46b32331959a04ec660f8034223d52a4f50e22
evanlay <evanlay@billericak12.com>
2019-09-25 12:29:35 -0400

    Commit MathEx Message

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -8,11 +8,10 @@ public class MathEx {
         int y = x%10;
         int z = (x/10)%10;
         int a = x/100;
-        int b = 
-        System.out.println(y);
-        System.out.println(z);
-        System.out.println(a);
-        System.out.print(b);
-        return x;
+        int b =
+        System.out.print(a);
+        System.out.print(y);
+        System.out.print(z);
+        return b ;
     }
     }
