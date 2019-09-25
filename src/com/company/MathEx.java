00d3bc565ad73e4c66c5c37639670f1a91cc11b5
evanlay <evanlay@billericak12.com>
2019-09-25 11:30:18 -0400

    Commit MathEx Message

---
+++ src/com/company/MathEx.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class MathEx {
+    public static void main(String[] args) {
+        swap(123);
+    }
+    public static int swap(int x) {
+        int y = x%10;
+        int z = (x/10)%10;
+        int a = x/100;
+        int b = 
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        System.out.print(b);
+        return x;
+    }
+    }
